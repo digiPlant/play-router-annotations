728d569e3706373aefdb30613a29cefbfc0374c1
Leon <leon@digiplant.se>
2011-04-13 22:57:51 +0200

    1.0

---
+++ src/play/modules/routerAnnotations/StaticRoutes.java
@@ -0,0 +1,15 @@
+package play.modules.routerAnnotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface StaticRoutes {
+
+   ServeStatic[] value();
+
+}
